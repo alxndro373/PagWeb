b6214834cee6ffeda5b3b6f3d359fe0f798063ec
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-23 23:52:44 -0600

    add component table

---
+++ client/src/administrador/components/Table.jsx
@@ -0,0 +1,40 @@
+import {updateCuidadRequest, deleteCuidadRequest} from '../../api/cuidades.api.js'
+
+const Table = ({title,cols,values}) => {
+    return (
+        <div className="container">
+        <h2 className="mt-4 mb-4 text-primary">{title}</h2>
+        <div className="border border-primary">
+            <table className="table table-primary">
+                <thead>
+                    
+                    <tr className="table-light">
+                           {
+                            Object.keys(cols).map((key,index) => (
+                                <th key={index}>{key}</th>
+                            ))
+                           }
+                    </tr>
+                    
+                </thead>
+                <tbody>
+                    {
+                        values.map((value,index) => (
+                            <tr key={index++}>
+                                {Object.keys(value).map((key,index) => (
+                                    
+                                    <td key={index}>{value[key]}</td>
+                                ))}
+                                <td><button onClick={async () => await deleteCuidadRequest(index)} className="btn btn-outline-danger">Eliminar</button></td>
+                                <td><button className="btn btn-outline-success">Editar</button></td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    )
+}
+export default Table
