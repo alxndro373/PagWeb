b5fff6af98a1c2b9eb1d117ea797065457b86ef3
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-27 12:47:46 -0600

    use custom hooks and components

--- client/src/administrador/pages/AutobusesForm.jsx
+++ client/src/administrador/pages/AutobusesForm.jsx
@@ -1,61 +1,30 @@
-import {Form, Formik} from 'formik'
-import {createAutobusRequest, updateAutobusRequest, deleteAutobusRequest} from '../../api/autobuses.api.js'
+import Nav from '../components/Navbar.jsx'
+import Foorm from '../components/Form.jsx'
+import Table from '../components/Table.jsx'
+import { useState } from 'react'
+import {useAutobus} from '../../hooks/useAutobus.js'
 
 
 export const AutobusesForm = () => {
-    let action = ""
+    const [action, setAction] = useState("")
+    const saveAction = () => {setAction('Guardar')}
+    const showAction = () => {setAction('Consultar')}
+    const {autobuses, fetchAutobuses,handleCreateAutobus, handleDeleteAutobus} = useAutobus()
+    let fields = {
+        idcamion: null,
+        placa: "",
+        numAsientos: 0
+    }
     return (
+        <>
+        <Nav />
         <div className='container'>
             <h1 className='text-primary  mt-4 mb-4'>Administrar Autobuses</h1>
             <div className='border border-primary' style={{backgroundColor: "#cfe2ff"}} >
-                <Formik
-                    initialValues={
-                        {
-                            idcamion : null,
-                            placa : "",
-                            numAsientos: 0,
-                            
-                        }
-                    }
-                    onSubmit={async (values) => {
-                        try {
-                            switch(action){
-                                case 'Guardar':
-                                    await createAutobusRequest(values)
-                                break;
-                                case 'Actualizar':
-                                    const {idcamion, ...res} = values
-                                    await updateAutobusRequest(idcamion, res)
-                                break;
-                                case 'Eliminar':
-                                    await deleteAutobusRequest(values.idcamion)
-                                break;
-                                
-                            }
-                        
-                        } catch (error) {
-                            console.log(error)
-                        }
-                    }}
-                >
-                {({handleChange, handleSubmit}) => (
-                    <Form onSubmit={handleSubmit}>
-                    <label className='form-label'>ID</label>
-                    <input className='form-control' type="text" name='idcamion' onChange={handleChange} placeholder="ID de Autobus"/>
-                    <label className='form-label'>Placa</label>
-                    <input className='form-control' type="text" name='placa' onChange={handleChange} placeholder="Placa"/>
-                    <label className='form-label'>Número de Asientos</label>
-                    <input className='form-control' type="text" name='numAsientos' onChange={handleChange} placeholder="Número de Asientos"/>
-                    <div className='grid text-center'>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit' onClick={() => action = 'Guardar'}>Guardar</button>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit'>Buscar</button>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit' onClick={() => action = 'Actualizar'}>Actualizar</button>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit' onClick={() => action = 'Eliminar'}>Eliminar</button>
-                    </div>
-                </Form>
-                )}
-                </Formik>
+               <Foorm fields={fields} handleCreate={handleCreateAutobus} fetch={fetchAutobuses} action={action} saveAction={saveAction} showAction={showAction} />
             </div>
         </div>
+        {action != "" ? <Table title={'Lista de Autobuses'} cols={fields} values={autobuses} onDelete={handleDeleteAutobus}/> : null}       
+        </>
     )
 }
--- client/src/administrador/pages/CuidadesForm.jsx
+++ client/src/administrador/pages/CuidadesForm.jsx
@@ -1,61 +1,34 @@
-import {Form, Formik} from 'formik'
-import {createCuidadRequest, updateCuidadRequest, deleteCuidadRequest} from '../../api/cuidades.api.js'
-
+import { useState} from 'react'
+import { useCuidad } from '../../hooks/useCuidad.js'
+import Table from '../components/Table.jsx'
+import Nav from '../components/Navbar.jsx'
+import Foorm from '../components/Form.jsx'
 
 export const CuidadesForm = () => {
-    let action = ""
+    
+    const [action, setAction] = useState("")
+    const saveAction = () => {setAction('Guardar')}
+    const showAction = () => {setAction('Consultar')}
+
+    const {cuidades, fetchCuidades, handleCreateCuidad,handleDeleteCuidad} = useCuidad()    
+    
+    let fields = {
+        idcuidad: null,
+        nombre: "",
+        estado: ""
+    }
     return (
+        <>
+        <Nav />
         <div className='container'>
             <h1 className='text-primary  mt-4 mb-4'>Administrar Cuidades</h1>
             <div className='border border-primary' style={{backgroundColor: "#cfe2ff"}} >
-                <Formik
-                    initialValues={
-                        {
-                            idcuidad : null,
-                            nombre : "",
-                            estado: "",
-                            
-                        }
-                    }
-                    onSubmit={async (values) => {
-                        try {
-                            switch(action){
-                                case 'Guardar':
-                                    await createCuidadRequest(values)
-                                break;
-                                case 'Actualizar':
-                                    const {idcuidad, ...res} = values
-                                    await updateCuidadRequest(idcuidad, res)
-                                break;
-                                case 'Eliminar':
-                                    await deleteCuidadRequest(values.idcuidad)
-                                break;
-                                
-                            }
-                        
-                        } catch (error) {
-                            console.log(error)
-                        }
-                    }}
-                >
-                {({handleChange, handleSubmit}) => (
-                    <Form onSubmit={handleSubmit}>
-                    <label className='form-label'>ID Cuidad</label>
-                    <input className='form-control' type="text" name='idcuidad' onChange={handleChange} placeholder="ID Cuidad"/>
-                    <label className='form-label'>Nombre</label>
-                    <input className='form-control' type="text" name='nombre' onChange={handleChange} placeholder="Nombre"/>
-                    <label className='form-label'>Estado</label>
-                    <input className='form-control' type="text" name='estado' onChange={handleChange} placeholder="Estado"/>
-                    <div className='grid text-center'>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit' onClick={() => action = 'Guardar'}>Guardar</button>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit'>Buscar</button>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit' onClick={() => action = 'Actualizar'}>Actualizar</button>
-                        <button className='m-3 btn btn-light btn-outline-primary ' type='submit' onClick={() => action = 'Eliminar'}>Eliminar</button>
-                    </div>
-                </Form>
-                )}
-                </Formik>
+                <Foorm fields={fields} handleCreate={handleCreateCuidad} fetch={fetchCuidades} action={action} saveAction={saveAction} showAction={showAction}
+                    />
             </div>
         </div>
+        
+        {action != "" ? <Table title={'Lista de Cuidades'} cols={fields} values={cuidades} onDelete={handleDeleteCuidad}/> : null}       
+        </>
     )
 }
