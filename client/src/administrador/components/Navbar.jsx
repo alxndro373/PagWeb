1a6c93c5f30621740d505b3990886104955cd817
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-28 12:00:02 -0600

    fix styles

--- client/src/administrador/components/Navbar.jsx
+++ client/src/administrador/components/Navbar.jsx
@@ -1,32 +1,36 @@
 import { Link } from "react-router-dom"
+import logo from "../assets/ADU.png"
 
 const Nav = () => {
     return (
         <>
-        <div>
-        <h1 className='bg-dark-subtle text-light text-center mb-0' data-bs-theme="dark">Administracion: Centro de Autobuses ADU</h1>
-        </div>
-        <nav className="navbar bg-dark" data-bs-theme="dark" >
-            <ul className="nav">
-                <li className="nav-item ">
+        
+        <nav className="navbar h-25" style={
+            {background: "linear-gradient(rgba(106, 164, 211, 0.948) ,rgb(253, 248, 248))",
+            boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)"}} >
+            
+            <ul className="nav d-flex align-items-center ">
+            <img src={logo} alt="" style={{width:150, height: "60px", objectFit:"cover"} } />
+                <li className="nav-item me-5">
                     <Link className="nav-link text-body" to="/">Inicio</Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item me-5">
                     <Link className="nav-link text-body" to="/administradorAutobus">Autobuses</Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item me-5">
                     <Link className="nav-link text-body" to="/newViaje">Viajes</Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item me-5">
                     <Link className="nav-link text-body" to="/administradorCuidad">Cuidades</Link>
                 </li>
-                <li className="nav-item">
+                <li className="nav-item me-5">
                     <Link className="nav-link text-body" to="/newUsuario">Usuario</Link>
                 </li>
                 <li className="nav-item">
                     <Link className="nav-link text-body" to="/newCompra">Compras</Link>
                 </li>
             </ul>
+            
         </nav>
         </>
     )
