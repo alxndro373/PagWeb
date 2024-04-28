0bc889533f4215f7d2ea3c6220118eb0873caa92
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-27 22:46:02 -0600

    update styles

--- client/src/administrador/components/Form.jsx
+++ client/src/administrador/components/Form.jsx
@@ -30,15 +30,15 @@ const Foorm = ({fields, handleCreate, fetch, action, saveAction, showAction}) =>
                 <Form onSubmit={handleSubmit}>
                     {
                         Object.keys(fields).map((ele,key) => (
-                            <div key={key}>
+                            <div className='container' key={key}>
                                 <label className='form-label'>{ele}</label>
-                                <input className='form-control' type="text" name={ele} onChange={handleChange} placeholder={ele} />
+                                <input className='form-control bg-light' type="text" name={ele} onChange={handleChange} placeholder={ele} />
                             </div>
                         ))
                     }
                     <div className='grid text-center'>
-                        <button className='m-3 btn btn-light btn-outline-primary' type='submit' onClick={() => saveAction()}>Guardar</button>
-                        <button className='m-3 btn btn-light btn-outline-primary' type='submit' onClick={() => showAction()}>Consultar</button>
+                        <button className='m-3 btn btn-light' type='submit' onClick={() => saveAction()}>Guardar</button>
+                        <button className='m-3 btn btn-light' type='submit' onClick={() => showAction()}>Consultar</button>
                     </div>
                 </Form>
             )}
