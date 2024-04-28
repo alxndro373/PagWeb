eff3d12449226396093aa29f449a606e29a7fd60
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-28 12:16:17 -0600

    use component HOme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import Nav from './administrador/components/Navbar.jsx'
+import {Home} from './administrador/pages/Home.jsx'
 
 export const App = () => {
   return (
     <>
-   
     <Nav />
+    <Home />
     </>
   )
 }
