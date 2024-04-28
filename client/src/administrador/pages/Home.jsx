17d08f99795428f7f81531882f1ebc5dabd2b1d6
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-28 11:58:37 -0600

    add line height

---
+++ client/src/administrador/pages/Home.jsx
@@ -0,0 +1,22 @@
+import autobus from '../assets/adu_bus.png'
+
+
+export const Home = () => {
+
+    return (
+        <>
+            <div className="container">
+                <section className='d-flex justify-content-between align-items-center'>
+                    <div>
+                        <h1 className='lh-1'>Central de Autobuses</h1>
+                        <h2 className='lh-lg fs-1' style={{marginLeft:"35%"}}>ADU</h2>
+                        <h4 className='text-center'>Bienvenido Administrador</h4>
+                    </div>
+                    <div>
+                        <img src={autobus} alt="" className='mb-5' style={{filter: "drop-shadow(-10px -10px 200px #0901e7)"}} />
+                    </div>
+                </section>
+            </div>
+        </>
+    )
+}
