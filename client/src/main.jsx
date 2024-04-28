ff68b1a16f2327daf98d23e03338534286445840
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-28 16:00:17 -0600

    import bootstrap-icon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import {App} from './App.jsx'
 import { CuidadesForm } from './administrador/pages/CuidadesForm.jsx'
 import { AutobusesForm } from './administrador/pages/AutobusesForm.jsx'
