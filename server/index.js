49e8f88bab65975660459f2686137fbe0c0c4863
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-28 17:59:54 -0600

    use usuario routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import {PORT} from './config.js'
 import cuidadesRoutes from './routes/cuidades.routes.js'
 import autobusesRoutes from './routes/autobuses.routes.js'
+import usuariosRoutes from './routes/usuarios.routes.js'
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(cuidadesRoutes)
 app.use(autobusesRoutes)
+app.use(usuariosRoutes)
 
 app.listen(PORT)
 
