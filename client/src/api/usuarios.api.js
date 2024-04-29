a0d40e96140b640bf24b2951925a79f3f54da354
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-28 22:12:17 -0600

    create usuario api

---
+++ client/src/api/usuarios.api.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const getUsuariosRequest = async () => await axios.get('http://localhost:4000/Usuarios')
+
+export const createUsuarioRequest = async (Usuario) => await axios.post('http://localhost:4000/Usuario', Usuario)
+
+export const updateUsuarioRequest = async (id,newFields) => await axios.put(`http://localhost:4000/Usuario/${id}`,newFields)
+
+export const deleteUsuarioRequest = async (id) => await axios.delete(`http://localhost:4000/Usuario/${id}`)
