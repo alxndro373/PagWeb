616813eb3ec17da7260473a0cb7a2954a2a09535
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-29 19:06:52 -0600

    add component viajes

--- client/src/administrador/pages/Viajes.jsx
+++ client/src/administrador/pages/Viajes.jsx
@@ -11,16 +11,15 @@ export const Viajes = () => {
     const showAction = () => {setAction('Consultar')}
     const {viajes, fetchViajes,handleCreateViaje, handleDeleteViaje} = useViaje()
     
-    let fields = {idViaje: null,Origen:"",Destino:"",fecha:"",hora:""}
-    let initialValues = {idViaje: null,idOrigen:null,idDestino:null,fecha:"",hora:""}
-    let holders = {idViaje:null,Origen:"",Destino:"","AAAA-MM-DD":"","HH-MM-SS":""}
+    let fields = {idViaje: null,Origen:"",Destino:"",idCamion:null,fecha:"",hora:""}
+    let holders = {idViaje:null,Origen:"",Destino:"",idCamion:null,"AAAA-MM-DD":"","HH-MM-SS":""}
     return (
         <>
         <Nav />
         <div className='container w-50 rounded-3' style={{backgroundColor:"#eef6f7"}}>
             <h1 className='text-dark text-center pt-4 m-4'>Administrar Viajes</h1>
             <div>
-               <Foorm initialValues={initialValues} fields={fields} holders={holders} handleCreate={handleCreateViaje} fetch={fetchViajes} action={action} saveAction={saveAction} showAction={showAction} />
+               <Foorm initialValues={fields} fields={fields} holders={holders} handleCreate={handleCreateViaje} fetch={fetchViajes} action={action} saveAction={saveAction} showAction={showAction} />
             </div>
         </div>
         {action != "" ? <Table title={'Lista de Viajes'} cols={fields} values={viajes} onDelete={handleDeleteViaje}/> : null}       
