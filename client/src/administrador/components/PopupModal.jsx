d2d95d41c1e291f37f352be99cd0747b3cbbe88e
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-04-30 00:52:28 -0600

    add component PopupModal

---
+++ client/src/administrador/components/PopupModal.jsx
@@ -0,0 +1,27 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import FormModal from './FormModal';
+
+function PopupModal({show, handleClose, initialValues, fields, holders, handleUpdate}) {
+  
+
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Actualizar</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+         <FormModal initialValues={initialValues} fields={fields} holders={holders} handleUpdate={handleUpdate}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default PopupModal
