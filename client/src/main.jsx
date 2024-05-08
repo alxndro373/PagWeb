43d14e9ffd803b90ec03458827fb007ddf15126c
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-05-08 00:42:17 -0600

    use router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,48 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import {App} from './App.jsx'
-import { Cuidades } from './administrador/pages/Cuidades.jsx'
-import { Autobuses } from './administrador/pages/Autobuses.jsx'
-import {Usuario} from './administrador/pages/Usuario.jsx'
-import {Viajes} from './administrador/pages/Viajes.jsx'
-import {Boletos} from './administrador/pages/Boleto.jsx'
-import ErrorPage  from './pages/ErrorPage.jsx'
+import {router} from './router/'
+import { AuthProvider } from './auth/AuthProvider'
 import {
-  createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "administradorCuidad",
-    element: <Cuidades />,
-  },
-  {
-    path: "administradorAutobus",
-    element: <Autobuses />
-  },
-  {
-    path: "administradorUsuario",
-    element: <Usuario />
-  },
-  {
-    path: "administradorViaje",
-    element: <Viajes />
-  },
-  {
-    path: "administradorBoleto",
-    element: < Boletos />
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
   </React.StrictMode>,
 )
