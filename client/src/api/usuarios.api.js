636af4b95b123c31ef3686cdf730a84282a2eeb7
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-05-08 00:47:07 -0600

    add request of function getPasswordAndName

--- client/src/api/usuarios.api.js
+++ client/src/api/usuarios.api.js
@@ -7,3 +7,5 @@ export const createUsuarioRequest = async (Usuario) => await axios.post('http://
 export const updateUsuarioRequest = async (id,newFields) => await axios.put(`http://localhost:4000/Usuario/${id}`,newFields)
 
 export const deleteUsuarioRequest = async (id) => await axios.delete(`http://localhost:4000/Usuario/${id}`)
+
+export const getPasswordAndNameRequest = async (correo) => await axios.get(`http://localhost:4000/usuario/correo/${correo}`)
