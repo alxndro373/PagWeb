6da9a2d5f23bc68fb807b6f5c89c9cf2a3ead244
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-05-08 00:41:57 -0600

    add router

---
+++ client/src/router/index.jsx
@@ -0,0 +1,65 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Autobuses } from "../administrador/pages/Autobuses";
+import { Cuidades } from "../administrador/pages/Cuidades";
+import { Usuario } from "../administrador/pages/Usuario";
+import { Viajes } from "../administrador/pages/Viajes";
+import { Boletos } from "../administrador/pages/Boleto";
+import ErrorPage from '../pages/ErrorPage'
+import { Register } from "../user/pages/Register";
+import { Login } from "../user/pages/Login";
+import { Home } from "../user/pages/Home";
+import {HomeAdmin} from "../administrador/pages/Home"
+import { Faq } from "../user/pages/Faq"
+import { ProtectedRoute } from "../administrador/routes/ProtectedRoute";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: "Atencion",
+        element: <Faq />
+    },
+    {
+        path: "Login",
+        element: <Login />
+    },
+    {
+        path: "Register",
+        element: <Register />
+    },
+    {
+        path: "/administrador",
+        element: <ProtectedRoute />,
+        children: [
+            {
+                path: "/administrador",
+                element: <HomeAdmin />,
+            },
+            {
+                path: "/administrador/cuidad",
+                element: <Cuidades />,
+            },
+            {
+                path: "/administrador/autobus",
+                element: <Autobuses />
+            },
+            {
+                path: "/administrador/usuario",
+                element: <Usuario />
+            },
+            {
+                path: "/administrador/viaje",
+                element: <Viajes />
+            },
+            {
+                path: "/administrador/boleto",
+                element: < Boletos />
+            },
+        ]
+    },
+
+])
