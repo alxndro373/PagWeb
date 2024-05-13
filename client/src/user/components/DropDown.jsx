ccf18fe823351e1ddfef004205dc905545ca1e73
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-05-12 20:27:14 -0600

    add dependecies for maps

--- client/src/user/components/DropDown.jsx
+++ client/src/user/components/DropDown.jsx
@@ -1,11 +1,15 @@
 import i1 from '../assets/busIcon.png'
 
-export const DropDown = ({ title, items }) => {
+export const DropDown = ({ title, items, onChange}) => {
+
+    const onSelectedChange = (e) => {
+        onChange(e.target.value)
+    }
 
     return (
         <div className='fw-bold' style={{ fontSize: "20px", background: "white", marginTop: "9%" }}>
             <label>{title}</label>
-            <select id={title} onChange={e => onChange(e.target.value)} style={{ width: "90%", borderRadius: "5px", border: "none" }}>
+            <select id={title} onChange={onSelectedChange} style={{ width: "90%", borderRadius: "5px", border: "none" }}>
                 {
                     items.map((item,key) => (
                         <option key={key} value={item.estado}>{item.estado}</option>
@@ -14,15 +18,6 @@ export const DropDown = ({ title, items }) => {
             </select>
             <img src={i1} />
         </div>
-        // <>
-        // <i className="bi bi-bus-front-fill"></i>
-        // <DropdownButton id="dropdown-basic-button" title={title} variant='secondary'>
-        //     {
-        //         items.map((item,key) => (
-        //             <Dropdown.Item key={key} >{item.value}</Dropdown.Item>
-        //         ))
-        //     }
-        //     </DropdownButton>
-        // </>
+        
     )
 }
