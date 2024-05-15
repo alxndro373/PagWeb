05a6007e517661a277309d354ea0e80f983f2e5d
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-05-15 01:18:04 -0600

    bug fix with update trip

--- server/controllers/viajes.controllers.js
+++ server/controllers/viajes.controllers.js
@@ -8,7 +8,7 @@ export const validOrigenAndDestino = async (req,res) => {
         if (origenRes.length === 0 || destinoRes.length === 0) return res.status(404).json({ message: "origen o  destino no econtrado" })
         const idOrigen = origenRes[0].idCiudad
         const idDestino = destinoRes[0].idCiudad
-        const [result] = await pool.query('SELECT hora,precio FROM viaje WHERE idOrigen = ? && idDestino = ?', [idOrigen,idDestino])
+        const [result] = await pool.query('SELECT fecha,hora,precio FROM viaje WHERE idOrigen = ? && idDestino = ?', [idOrigen,idDestino])
         res.json(result)  
     } catch (error) {
         console.log(error)
@@ -58,12 +58,12 @@ export const createViaje = async (req, res) => {
 
 export const updateViaje = async (req, res) => {
     try {
-        const {idViaje,Origen,Destino,idCamion,fecha,hora} = req.body
+        const {Origen,Destino,idCamion,fecha,hora} = req.body
         const [origenRes] = await pool.query('SELECT idCiudad FROM ciudad where estado = ?', [Origen])
         const idOrigen = origenRes[0].idCiudad
         const [destinoRes] = await pool.query('SELECT idCiudad FROM ciudad where estado = ?', [Destino])
         const idDestino = destinoRes[0].idCiudad
-        const [result] = await pool.query('UPDATE viaje SET idViaje=?,idOrigen=?,idDestino=?,idCamion=?,fecha=?,hora=? WHERE idViaje = ?', [idViaje,idOrigen,idDestino,idCamion,fecha,hora,req.params.id])
+        const [result] = await pool.query('UPDATE viaje SET idViaje=?,idOrigen=?,idDestino=?,idCamion=?,fecha=?,hora=? WHERE idViaje = ?', [req.params.id,idOrigen,idDestino,idCamion,fecha,hora,req.params.id])
         res.json(result)
         console.log(result)
     } catch (error) {
