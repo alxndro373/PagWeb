d474fa92d823ac4f9b29b07397b531d3788fc942
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-05-15 19:17:22 -0600

    add new routes

--- server/routes/boletos.routes.js
+++ server/routes/boletos.routes.js
@@ -4,13 +4,19 @@ import {
     getBoleto,
     createBoleto,
     updateBoleto,
-    deleteBoleto
+    deleteBoleto,
+    getBoletoByUser,
+    getOriginAndDestination,
+    getAsientoByViaje
 
 } from '../controllers/boletos.controllers.js'
 
 const router = Router()
 
 router.get('/boletos', getBoletos)
+router.get('/boletos/:idUsuario', getBoletoByUser)
+router.get('/boletos/viaje/:id', getOriginAndDestination)
+router.get('/boletos/asientos/:idViaje',getAsientoByViaje)
 router.get('/boleto/:id', getBoleto)
 router.post('/boleto', createBoleto)
 router.put('/boleto/:id', updateBoleto)
