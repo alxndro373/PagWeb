afce83174815ad2538a6b90429654b5b75383267
AleandroSm <l21021167@veracruz.tecnm.mx>
2024-05-15 19:18:02 -0600

    add new request

--- client/src/api/boletos.api.js
+++ client/src/api/boletos.api.js
@@ -7,3 +7,9 @@ export const createBoletoRequest = async (boleto) => await axios.post('http://lo
 export const updateBoletoRequest = async (id,newFields) => await axios.put(`http://localhost:4000/boleto/${id}`,newFields)
 
 export const deleteBoletoRequest = async (id) => await axios.delete(`http://localhost:4000/boleto/${id}`)
+
+export const getBoletosByUserRequest = async (idUsuario) => await axios.get(`http://localhost:4000/boletos/${idUsuario}`)
+
+export const getOriginAndDestinationRequest = async (idViaje) => await axios.get(`http://localhost:4000/boletos/viaje/${idViaje}`)
+
+export const getAsientoByViajeRequest = async (idViaje) => await axios.get(`http://localhost:4000/boletos/asientos/${idViaje}`)
